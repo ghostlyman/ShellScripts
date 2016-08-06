21a71b550118c6a85bfb3933032d927ed9818e2d
Rick Xu <rickxu1989@gmail.com>
2016-08-06 17:55:40 +0800

    Correct typos issue and add Manually VERSION control feature.

--- pycharm.sh
+++ pycharm.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
-read -p 'https://www.jetbrains.com/pycharm/download/#section=linux'
-VERSION="2016.2"
+
+echo 'Please go to below URL to check the newest version of PyCharm in CentOS platform\n
+https://www.jetbrains.com/pycharm/download/#section=linux'
+
+echo "Please input the version you want to install: "
+read -p "(Default version: 2016.2)" VERSION
+
+[ -z "$VERSION" ] && VERSION="2016.2"
+echo
+echo "You are go to install $VERSION version of PyCharm Community in your System."
+echo "Please wait a few minutes."
+echo
 
 if [[ $EUID -ne 0 ]]; then
-    echo "Please run as root priviledge"
+    echo "Please run as root privilege"
     exit 1
 fi
 
