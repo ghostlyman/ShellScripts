6153e5cf1ada6e9c5f8ad9db3cbb8d662a4bbe06
Rick Xu <rickxu1989@gmail.com>
2016-08-07 11:30:22 +0800

    Create pyenv.sh

---
+++ pyenv.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [ $EUID -ne 0 ]; then
+    echo "Please run as root privilege"
+    exit 1
+fi
+
+# Install git tools
+yum -y install git
+
+# Clone pyenv to root local home directory
+cd ~
+git clone git://github.com/yyuu/pyenv.git .pyenv
+
+#Provide pyenv variable environment
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+# Reload Shell instance.
+exec $SHELL
+
+# Install pyenv-virtualenv plugin
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+# REload Shell instance.
+exec $SHELL
+
