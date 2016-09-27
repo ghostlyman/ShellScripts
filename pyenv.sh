c6c0198167ab2086b336787f729d3953d4a74a01
Rick Xu <rickxu1989@gmail.com>
2016-09-27 16:01:16 +0800

    Updated pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -2,17 +2,20 @@
 
 # Only Root privilege can run this script
 if [ $EUID -ne 0 ]; then
-    echo "Please run as root privilege"
+    echo "Error..........Please run as root privilege"
     exit 1
 fi
+
 
 # Install git tools
 yum -y install git gcc openssl openssl-devel
 yum -y groupinstall "Development tools"
 
+
 # Clone pyenv to root local home directory
 cd ~
 git clone git://github.com/yyuu/pyenv.git .pyenv
+
 
 #Provide pyenv variable environment
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
@@ -22,6 +25,7 @@ echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 # Install pyenv-virtualenv plugin
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
 
 # REload Shell instance.
 exec $SHELL
