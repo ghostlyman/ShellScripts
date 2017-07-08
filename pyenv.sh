3bdaa24529616adbeb7568aac45c98edd12f0ff8
root <root@mail.xurick.com>
2017-07-08 11:19:52 +0000

    Updated pyenv virtualenv prompt feature.

--- pyenv.sh
+++ pyenv.sh
@@ -56,6 +56,7 @@ export_env() {
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
     echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+    eval "$(pyenv virtualenv-init -)" >> ~/.bashrc
 }
 
 
